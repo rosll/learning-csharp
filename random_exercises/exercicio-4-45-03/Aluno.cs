3e98d178b7feb9976c29c27d1c5c222e7d98edb4
Romulo Lima <romulosilva-07@hotmail.com>
2021-11-10 22:41:42 -0300

    finalizando 4ªseção de exercícios

---
+++ random_exercises/exercicio-4-45-03/Aluno.cs
@@ -0,0 +1,33 @@
+﻿namespace exercicio_4_45_03
+{
+    public class Aluno
+    {
+        public string Nome;
+        public double Nota1, Nota2, Nota3;
+
+        public double NotaTotal()
+        {
+            return Nota1 + Nota2 + Nota3;
+        }
+
+        public bool Aprovado()
+        {
+            if (NotaTotal() >= 60.0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public double PontoPendente()
+        {
+            if (Aprovado())
+            {
+                return 0.0;
+            }
+
+            return 60.0 - NotaTotal();
+        }
+    }
+}
---
+++ random_exercises/exercicio-4-45-03/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace exercicio_4_45_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno = new Aluno();
+
+            Console.Write("Nome do aluno: ");
+            aluno.Nome = Console.ReadLine();
+
+            Console.WriteLine("Digite as três notas do aluno: ");
+            aluno.Nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.Nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.Nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine($"NOTA FINAL = {aluno.NotaTotal().ToString("F2", CultureInfo.InvariantCulture)}");
+
+            if (aluno.Aprovado())
+            {
+                Console.WriteLine("APROVADO");
+            }
+            else
+            {
+                Console.WriteLine("REPROVADO");
+                Console.WriteLine($"FALTARAM {aluno.PontoPendente().ToString("F2", CultureInfo.InvariantCulture)} PONTOS");
+            }
+        }
+    }
+}
