616f1e31bd71d8d6a4269a60c0312d2ca3eda362
Romulo Lima <romulosilva-07@hotmail.com>
2021-11-11 19:33:04 -0300

    + membros estáticos

---
+++ random_exercises/exercicio-4-48-01/ConversorDeMoeda.cs
@@ -0,0 +1,13 @@
+﻿namespace exercicio_4_48_01
+{
+    public class ConversorDeMoeda
+    {
+        public static double iof = 6.0;
+
+        public static double Calculo(double cotacao, double dolar)
+        {
+            double result = cotacao * dolar;
+            return result + result * iof / 100;
+        }
+    }
+}
---
+++ random_exercises/exercicio-4-48-01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace exercicio_4_48_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual é a cotação do dólar? ");
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantos dólares voce vai comprar? ");
+            double dolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double total = ConversorDeMoeda.Calculo(cotacao, dolar);
+            Console.WriteLine($"Valor a ser pago em reais = {total.ToString("F2", CultureInfo.InvariantCulture)}");
+
+        }
+    }
+}
