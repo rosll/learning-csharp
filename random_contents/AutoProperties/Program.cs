25dff7e31bf02bd22f34db0a88cda29cd37beb23
Romulo Lima <romulosilva-07@hotmail.com>
2021-11-13 20:36:46 -0300

    Construtores, Encapsulamento e Auto Properties

---
+++ random_contents/AutoProperties/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Construct {
+    class Program {
+        static void Main(string[] args)
+        {
+
+            Produto p = new Produto("Tv", 500.00, 10);
+            
+            p.Nome = "T";
+
+            Console.WriteLine(p.Nome);
+            Console.WriteLine(p.Preco);
+        }
+    }
+}
---
+++ random_contents/Encapsulamento/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Construct {
+    class Program {
+        static void Main(string[] args)
+        {
+
+            Produto p = new Produto("Tv", 500.00, 10);
+            
+            p.SetNome("T");
+
+            Console.WriteLine(p.GetNome());
+            Console.WriteLine(p.GetPreco());
+        }
+    }
+}
